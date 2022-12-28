a12443cdd6f3634c413d13f1cae50c54644eb21b
Dimitar Tsvetanov? <dimitarcho93@gmail.com>
2022-12-28 20:48:16 +0200

    Add Login, Register routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import {
+    Box,
+    CssBaseline,
+    Paper,
+    Typography,
+    Toolbar,
+    AppBar,
+    IconButton,
+    Button,
+} from "@mui/material";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        ></IconButton>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            <Button
+                                color="inherit"
+                                onClick={() => navigate("/")}
+                            >
+                                Bulgarian Medieval History
+                            </Button>
+                        </Typography>
+                        <Button
+                            color="inherit"
+                            onClick={() => navigate("/login")}
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            color="inherit"
+                            onClick={() => navigate("/register")}
+                        >
+                            Register
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
